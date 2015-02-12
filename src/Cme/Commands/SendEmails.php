b11c56bf76d45c813598fe10aed8d6977a7d0a86
Oke Ugwu <oke.ugwu@justdevelop.it>
2015-02-12 17:44:59 +0000

    improved sendEmails to be sticky and also update campaign status from "Sending" to "Sent" accordingly

--- src/Cme/Commands/SendEmails.php
+++ src/Cme/Commands/SendEmails.php
@@ -32,13 +32,14 @@ class SendEmails extends Command
       // get data as utf8
       $this->_dbConn->query('SET CHARACTER SET utf8');
 
+      $lockedCampaignId = null;
       while(true)
       {
         //read jobs from queue
         $query = $this->_dbConn->query(
           sprintf(
             'SELECT * FROM %s WHERE locked_by="%s"
-            AND status ="%s" ORDER BY send_priority DESC LIMIT %d',
+            AND `status` ="%s" ORDER BY send_priority DESC LIMIT %d',
             $this->_queueTable,
             $instanceName,
             'pending',
@@ -66,10 +67,15 @@ class SendEmails extends Command
             var_dump($emailSent);
             var_dump($message);die;
 
+            if($lockedCampaignId == null)
+            {
+              $lockedCampaignId = $message->campaign_id;
+            }
+
             //unlock message and set the appropriate status
             $status = ($emailSent) ? 'Sent' : 'Failed';
             $sql    = sprintf(
-              "UPDATE %s SET locked_by=NULL, status='%s'
+              "UPDATE %s SET locked_by=NULL, `status`='%s'
               WHERE id=%d",
               $this->_queueTable,
               $status,
@@ -97,31 +103,98 @@ class SendEmails extends Command
         }
         else
         {
-          echo "Locking some rows" . PHP_EOL;
-          //lock some messages
-          $sql = sprintf(
-            "UPDATE %s SET locked_by='%s'
+          if($lockedCampaignId == null)
+          {
+            //pick a campaign and stick to it
+            $sql = sprintf(
+              "UPDATE %s SET locked_by='%s'
+               WHERE locked_by IS NULL
+               AND send_time < %d
+               AND `status`='%s'
+               ORDER BY send_priority DESC
+               LIMIT %d",
+              $this->_queueTable,
+              $instanceName,
+              time(),
+              'Pending',
+              1
+            );
+            $this->_dbConn->query($sql);
+
+            $query = $this->_dbConn->query(
+              sprintf(
+                'SELECT campaign_id FROM %s WHERE locked_by="%s"
+                AND `status` ="%s" ORDER BY send_priority DESC LIMIT 1',
+                $this->_queueTable,
+                $instanceName,
+                'pending'
+              )
+            );
+
+            $message = $query->fetch_object();
+            if($message)
+            {
+              $lockedCampaignId = $message->campaign_id;
+            }
+          }
+
+          if($lockedCampaignId)
+          {
+            echo "Locking some rows for campaign $lockedCampaignId" . PHP_EOL;
+            //lock some messages
+            $sql = sprintf(
+              "UPDATE %s SET locked_by='%s'
             WHERE locked_by IS NULL
+            AND campaign_id = %d
             AND send_time < %d
-            AND status='%s'
+            AND `status`='%s'
             ORDER BY send_priority DESC
             LIMIT %d",
-            $this->_queueTable,
-            $instanceName,
-            time(),
-            'Pending',
-            $this->batchSize
-          );
-          $this->_dbConn->query($sql);
+              $this->_queueTable,
+              $instanceName,
+              $lockedCampaignId,
+              time(),
+              'Pending',
+              $this->batchSize
+            );
+            $this->_dbConn->query($sql);
+            $sleep = ($this->_dbConn->affected_rows == 0);
+          }
+          else
+          {
+            $sleep = true;
+          }
+
           //if process could not lock any rows. Lets take a break
           //to avoid overloading the server
-          if($this->_dbConn->affected_rows == 0)
+          if($sleep)
           {
+            //set status of campaign to sending
+            $sql = sprintf(
+              "UPDATE campaigns SET `status`='%s'
+               WHERE id = %d",
+              'Sent',
+              $lockedCampaignId
+            );
+            $this->_dbConn->query($sql);
+
+            $lockedCampaignId = null;
             sleep(5);
             echo @date('Y-m-d H:i:s') . ": Sleeping for a bit" . PHP_EOL;
           }
+          else
+          {
+            //set status of campaign to sending
+            $sql = sprintf(
+              "UPDATE campaigns SET `status`='%s'
+               WHERE id = %d",
+              'Sending',
+              $lockedCampaignId
+            );
+            $this->_dbConn->query($sql);
+          }
         }
-      };
+      }
     }
     else
     {
